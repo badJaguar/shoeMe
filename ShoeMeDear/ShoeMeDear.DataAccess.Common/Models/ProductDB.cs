88257202b2abb82ba8686f4fdce13290c79bc134
Mikhail <mikhailshishlyannikov@gmail.com>
2018-10-23 20:33:10 +0300

    xml-documentation for ProductDB

--- ShoeMeDear/ShoeMeDear.DataAccess.Common/Models/ProductDB.cs
+++ ShoeMeDear/ShoeMeDear.DataAccess.Common/Models/ProductDB.cs
@@ -6,18 +6,39 @@ using System.Threading.Tasks;
 
 namespace ShoeMeDear.DataAccess.Common.Models
 {
+    /// <summary>
+    /// Model for Product entity is used in database
+    /// </summary>
     public class ProductDB
     {
+        /// <summary>
+        /// Unique identifier of concrete Product in database.
+        /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// Unique name of concrete Product in database.
+        /// </summary>
         public string Name { get; set; }
 
-        public string PathPicture { get; set; }
+        /// <summary>
+        /// Product picture path.
+        /// </summary>
+        public string PicturePath { get; set; } //Возможно придется сделать модельку для картинок
 
+        /// <summary>
+        /// Defines the Product description
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Specifies the timestamp of creation of concrete Product in database.
+        /// </summary>
         public DateTime Created { get; set; }
 
-        public DateTime Modified { get; set; }
+        /// <summary>
+        /// Specifies the timestamp of modifying of any property of the Product in database.
+        /// </summary>
+        public DateTime? Modified { get; set; }
     }
 }
