354deb59e9c8f7d61479b11383222127095571a9
Mikhail <mikhailshishlyannikov@gmail.com>
2018-10-23 20:06:26 +0300

    ProductDB is implemented

---
+++ ShoeMeDear/ShoeMeDear.DataAccess.Common/Models/ProductDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeMeDear.DataAccess.Common.Models
+{
+    public class ProductDB
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PathPicture { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Modified { get; set; }
+    }
+}
