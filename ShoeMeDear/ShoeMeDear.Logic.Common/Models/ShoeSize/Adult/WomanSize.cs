1a77fa81846a81907bb24a632cb46504743efe54
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-24 16:18:54 +0300

    Added woman's adult size model into BL.

---
+++ ShoeMeDear/ShoeMeDear.Logic.Common/Models/ShoeSize/Adult/WomanSize.cs
@@ -0,0 +1,30 @@
+﻿namespace ShoeMeDear.Logic.Common.Models.ShoeSize.Adult
+{
+    public class WomanSize
+    {
+        /// <summary>
+        /// Gets or sets adult woman's size (USA standard).
+        /// </summary>
+        public double Us { get; set; }
+
+        /// <summary>
+        /// Gets or sets adult woman's size (Euro Union standard).
+        /// </summary>
+        public double Eu { get; set; }
+
+        /// <summary>
+        /// Gets or sets adult woman's size (United Kingdom standard).
+        /// </summary>
+        public double Uk { get; set; }
+
+        /// <summary>
+        /// Gets or sets adult woman's in inches.
+        /// </summary>
+        public double Inches { get; set; }
+
+        /// <summary>
+        /// Gets or sets adult woman's in centimeters.
+        /// </summary>
+        public double Cm { get; set; }
+    }
+}
