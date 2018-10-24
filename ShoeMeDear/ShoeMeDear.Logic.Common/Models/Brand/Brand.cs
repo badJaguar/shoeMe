eadd5e3b748ec90fe796cdb4e81e3ecd2841e3a9
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-10-24 15:18:10 +0300

    Added Brand and BrandDb models into DAL and BL.

---
+++ ShoeMeDear/ShoeMeDear.Logic.Common/Models/Brand/Brand.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoeMeDear.Logic.Common.Models.Logo
+{
+    public class Brand
+    {
+        /// <summary>
+        /// Gets or sets unique identifier of website brand.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a brand name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
