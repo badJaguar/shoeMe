ac03be0dbe03a713c35aeed9ca0072484d1d4ecd
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-11-07 23:17:09 +0300

    Shoe size and gender models added into DAL

--- ShoeMeDear/ShoeMeDear.DataAccess.Common/Models/Sizes/ShoeSizeDb.cs
+++ ShoeMeDear/ShoeMeDear.DataAccess.Common/Models/Sizes/ShoeSizeDb.cs
@@ -1,7 +1,32 @@
-﻿namespace ShoeMeDear.DataAccess.Common.Models.Sizes
+﻿using System;
+
+namespace ShoeMeDear.DataAccess.Common.Models.Sizes
 {
     public class ShoeSizeDb
     {
-        
+        /// <summary>
+        /// Gets or sets gender and age category.
+        /// </summary>
+        public string Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets unique identifier of a size.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of a size.
+        /// </summary>
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets shoes size modifying date time.
+        /// </summary>
+        public DateTime? Modified { get; set; }
+
+        /// <summary>
+        /// Gets or sets shoes size modifying date time.
+        /// </summary>
+        public DateTime Created { get; set; }
     }
 }
--- ShoeMeDear/ShoeMeDear.Logic.Common/Models/Gender.cs
+++ ShoeMeDear/ShoeMeDear.Logic.Common/Models/Gender.cs
@@ -11,5 +11,10 @@
         /// Gets or sets the unique identifier.
         /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag is size was chosen or not.
+        /// </summary>
+        public bool Added { get; set; }
     }
 }
