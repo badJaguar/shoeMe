1feb8195a294103c3549c65684fc60694d31e0f2
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2018-11-08 14:40:00 +0300

    Shop and ShopDb models added

---
+++ ShoeMeDear/ShoeMeDear.Logic.Common/Models/Shop.cs
@@ -0,0 +1,23 @@
+﻿namespace ShoeMeDear.Logic.Common.Models
+{
+    /// <summary>
+    /// Represents a data of included into website shop, store or any marketplace.
+    /// </summary>
+    public class Shop
+    {
+        /// <summary>
+        /// Gets or sets marketplace ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets marketplace name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference of a marketplace.
+        /// </summary>
+        public string Reference { get; set; }
+    }
+}
